f8a1f246b1d05219a967c18e696e61f915da2c31
Gabriel de Castro <108163958+kkgabrielcas@users.noreply.github.com>
2025-04-01 20:45:13 -0300

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -3,4 +3,4 @@ import {createClient} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+
 const SUPABASE_URL = 'https://oytmvgfxzawvwiabripq.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95dG12Z2Z4emF3dndpYWJyaXBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMzNjkyMDksImV4cCI6MjA1ODk0NTIwOX0.XoGQUiLJ2NsnopX7Fhx7qgxbdClOO_cPG6_p0I_cHso';
 
-const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
